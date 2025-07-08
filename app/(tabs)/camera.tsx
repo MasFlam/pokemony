b241fca1abcb4ef372884cb085171857fbd6053e
Łukasz Drukała <lukasz.drukala@swmansion.com>
2025-07-08 11:07:13 +0200

    camera swapping

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,10 +1,14 @@
 import { PokeImage } from "@/components/PokeImage";
 import { TextButton } from "@/components/ui/Button";
 import { useAppSelector } from "@/state/hooks";
+import { PlatformPressable } from "@react-navigation/elements";
+import { useColorScheme } from "nativewind";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { ArrowUturnLeftIcon } from "react-native-heroicons/solid";
 import {
   Camera,
+  CameraPosition,
   runAsync,
   useCameraDevice,
   useCameraFormat,
@@ -16,10 +20,13 @@ import {
   useFaceDetector,
 } from "react-native-vision-camera-face-detector";
 import { Worklets } from "react-native-worklets-core";
+import colors from "tailwindcss/colors";
 
 export default function CameraLayout() {
+  const { colorScheme } = useColorScheme();
   const { hasPermission, requestPermission } = useCameraPermission();
-  const cameraDevice = useCameraDevice("back");
+  const [cameraPosition, setCameraPosition] = useState<CameraPosition>("front");
+  const cameraDevice = useCameraDevice(cameraPosition);
   const cameraFormat = useCameraFormat(cameraDevice, [{ fps: 10 }]);
   const { detectFaces } = useFaceDetector({ minFaceSize: 0.05 });
   const [facePos, setFacePos] = useState<[x: number, y: number] | undefined>();
@@ -29,12 +36,20 @@ export default function CameraLayout() {
     await requestPermission();
   };
 
+  const swapCamera = () => {
+    if (cameraPosition === "front") {
+      setCameraPosition("back");
+    } else {
+      setCameraPosition("front");
+    }
+  };
+
   const handleFaces = Worklets.createRunOnJS((faces: Face[]) => {
     // console.log("Detected faces: ", faces);
     for (const face of faces) {
       const { x, y, width, height } = face.bounds;
-      const centerX = x + (0 * width) / 2 - 50;
-      const centerY = y + (0 * height) / 2 - 50;
+      const centerX = x + (-1 * width) / 2 - 0 * 50;
+      const centerY = y + (-1 * height) / 2 - 0 * 50;
       console.log(`Face at (${x}, ${y}) --> (${centerX}, ${centerY})`);
       setFacePos([centerX, centerY]);
     }
@@ -79,7 +94,7 @@ export default function CameraLayout() {
         device={cameraDevice}
         isActive={true}
         frameProcessor={frameProcessor}
-        fps={10}
+        fps={20}
         format={cameraFormat}
       />
       {favPokeName && (
@@ -93,6 +108,20 @@ export default function CameraLayout() {
           <PokeImage pokemonName={favPokeName} />
         </View>
       )}
+      <PlatformPressable
+        android_ripple={{ foreground: true }}
+        style={{
+          position: "absolute",
+          zIndex: 2,
+        }}
+        className="overflow-hidden absolute right-5 bottom-5 py-3 px-4 rounded-full bg-zinc-50 dark:bg-zinc-800"
+        onPress={swapCamera}
+      >
+        <ArrowUturnLeftIcon
+          size={24}
+          color={colorScheme === "light" ? colors.black : colors.white}
+        />
+      </PlatformPressable>
     </View>
   );
 }
